0adf3e21014dd7d0bc105797f288fe8cc824c1f0
Sebastien Destannes <sdestann@c2r5p2.42perpignan.fr>
2023-05-16 14:32:15 +0200

    README.md for test.sh

--- Testeurs/Push_swap/test.sh
+++ Testeurs/Push_swap/test.sh
@@ -5,16 +5,16 @@ NBR=0;
 #modification ici :#
 MINI=-100000;
 MAXI=100000;
-NBR_OF_TESTS=100;
+NBR_OF_NBRS=100;
 
 
 while [ $ITER -gt 0 ]
 do
 	echo $ITER;
-   NBR=$(ARG=$(seq $(( $MINI )) $MAXI | shuf -n $NBR_OF_TESTS | xargs); ./push_swap $ARG | wc -l);
+   NBR=$(ARG=$(seq $(( $MINI )) $MAXI | shuf -n $NBR_OF_NBRS | xargs); ./push_swap $ARG | wc -l);
    MOYENNE=$(( MOYENNE + NBR ));
    ITER=$((ITER-1));
 done
-MOYENNE=$(( MOYENNE / $NBR_OF_TESTS));
+MOYENNE=$(( MOYENNE / $NBR_OF_NBRS));
 
 echo "Votre moyenne est $MOYENNE";
